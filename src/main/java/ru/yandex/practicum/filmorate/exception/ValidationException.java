1ceb92480c38e8b61173f61345f0af6fe6b61784
Романевский <V9139493364@yandex.ru>
2025-08-06 20:48:07 +0700

    Проект 10 спринта

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
+/**
+ * Исключения для валидации
+ */
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
