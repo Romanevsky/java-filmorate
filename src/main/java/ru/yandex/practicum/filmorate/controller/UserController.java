7004bd969d631c264bb88d5438490943957f9f29
Романевский <V9139493364@yandex.ru>
2025-08-31 14:38:21 +0700

    Revert "правка1"
    This reverts commit eb71dc8ffef212b032a2801fdc8a0df32815e53f.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -82,8 +82,6 @@ public class UserController {
     @DeleteMapping("/{id}/friends/{friendId}")
     public ResponseEntity<?> removeFriend(@PathVariable int id, @PathVariable int friendId) {
         try {
-            User user = userService.getById(id);
-            User friend = userService.getById(friendId);
             userService.removeFriend(id, friendId);
             return ResponseEntity.ok().build();
         } catch (NoSuchElementException e) {
@@ -96,7 +94,6 @@ public class UserController {
     @GetMapping("/{id}/friends")
     public ResponseEntity<?> getFriends(@PathVariable int id) {
         try {
-            User user = userService.getById(id);
             List<User> friends = userService.getFriends(id);
             return ResponseEntity.ok(friends);
         } catch (NoSuchElementException e) {
